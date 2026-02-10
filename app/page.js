a236d73c057573945efedb2df91e5b1a691ab2cf
otomehcoderobtings-ship-it <o.tomeh.code.robtings@gmail.com>
2026-02-10 18:27:33 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "Arial", padding: 30 }}>
+      <h1>O_Tomeh AI</h1>
+      <p>If you can see this page, the app files are in place ✅</p>
+      <a href="/auth">Go to Login</a>
+    </main>
+  );
+}
